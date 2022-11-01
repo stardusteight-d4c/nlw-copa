301ccf42246ed6227af2121a954f505cc390a77e
Gabriel Sena <stardusteight.d4cc@gmail.com>
2022-11-01 15:45:15 -0300

    interface and get and post data with axios

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      roboto: 'Roboto, sans-serif',
+    },
+    extend: {
+      backgroundImage: {
+        app: 'url(/bg-effects.png)'
+      },
+      colors: {
+        ignite: {
+          500: '#129E57'
+        },
+        yellow: {
+          500: '#F7DD43'
+        },
+        gray: {
+          100: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214'
+        }
+      }
+    },
   },
   plugins: [],
 }
